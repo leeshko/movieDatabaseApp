fd8af2e37f82e7c66834f5e1453b7793822ca6e4
Dmitri Lishko <dmitrilishko@gmail.com>
2025-05-01 22:47:27 -0600

    fix config 1

--- next.config.ts
+++ next.config.ts
@@ -4,17 +4,17 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["image.tmdb.org"],
   },
-  serverExternalPackages: ["mongoose"],
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = [...config.externals, "mongoose"];
-    }
-    config.experiments = {
-      topLevelAwait: true,
-      layers: true,
-    };
-    return config;
-  },
+  // serverExternalPackages: ["mongoose"],
+  // webpack: (config, { isServer }) => {
+  //   if (isServer) {
+  //     config.externals = [...config.externals, "mongoose"];
+  //   }
+  //   config.experiments = {
+  //     topLevelAwait: true,
+  //     layers: true,
+  //   };
+  //   return config;
+  // },
 };
 
 export default nextConfig;
